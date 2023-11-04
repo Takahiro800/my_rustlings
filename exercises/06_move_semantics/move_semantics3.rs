c966bfe8c87f7c112f508e500c5b505839395261
taemath <hirotaka081630@gmail.com>
2023-11-04 17:11:26 +0900

    feat: 06_move_semantics

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -6,18 +6,20 @@
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(vec0);
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
+    // NOTE: ここではvec0はmoveされているので、vec0は使えない
+    // println!("{:?}", vec0);
+    // q: 所有権を戻すにはどうすればいいのか？
+    // a: 戻すことはできない。関数の引数として渡すことで、所有権を渡すことができる
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
