d9ae0895e307d71115ad9507a3c3e22121ad3038
taemath <hirotaka081630@gmail.com>
2023-11-14 04:43:06 +0900

    feat(20): threads

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -3,10 +3,7 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
@@ -27,22 +24,24 @@ impl Queue {
 }
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+    let tx1 = mpsc::Sender::clone(&tx);
+    let tx2 = mpsc::Sender::clone(&tx);
+
+    let first_half = q.first_half;
+    let second_half = q.second_half;
 
     thread::spawn(move || {
-        for val in &qc1.first_half {
+        for val in first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
-        for val in &qc2.second_half {
+        for val in second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
